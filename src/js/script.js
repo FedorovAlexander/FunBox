33862c80e5008eae98c3797b1282732a3eb90b05
alex <hamster2240@gmail.com>
2018-06-23 22:34:46 +0300

    New classes for card__text items

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,10 @@
-$(document).ready(function(){
-  // весь ваш код
-});
+const cards = document.querySelectorAll('.cards__item');
+const weight = document.querySelectorAll('.food-card__weight')
+console.log(weight)
+cards.forEach(function(card) {
+  const weight = card.querySelector('.food-card__weight')
+  card.addEventListener('click', function() {
+    this.classList.toggle('food-card--selected')
+    weight.classList.toggle('food-card__weight--selected')
+  })
+})
