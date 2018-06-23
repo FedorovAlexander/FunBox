c17ad1d9aeb6b846f10f7682732ba723b0bc9a0a
alex <hamster2240@gmail.com>
2018-06-23 22:46:57 +0300

    Functions for active card state

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,17 @@
 const cards = document.querySelectorAll('.cards__item');
-const weight = document.querySelectorAll('.food-card__weight')
-console.log(weight)
+const buyLink = document.querySelectorAll('.cards__text-link');
+const cardText = document.querySelectorAll('.cards__text')
+buyLink.forEach(function(link, i) {
+  link.addEventListener('click', function() {
+    var weight = cards[i].querySelector('.food-card__weight')
+    cards[i].classList.toggle('food-card--selected')
+    weight.classList.toggle('food-card__weight--selected')
+  })
+})
+
 cards.forEach(function(card) {
   const weight = card.querySelector('.food-card__weight')
+  const link = card.querySelector('.cards__text-link')
   card.addEventListener('click', function() {
     this.classList.toggle('food-card--selected')
     weight.classList.toggle('food-card__weight--selected')
