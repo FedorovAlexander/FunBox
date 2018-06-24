77f1e970fb3794347c4af7935585291c842ee96c
alex <hamster2240@gmail.com>
2018-06-24 13:11:19 +0300

    Modify click function. Add condition for checking disable card state

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
 const cards = document.querySelectorAll('.cards__item');
 const buyLink = document.querySelectorAll('.cards__text-link');
-const cardText = document.querySelectorAll('.cards__text')
-const stuff = document.querySelectorAll('.cards__text--stuffing')
-const buy = document.querySelectorAll('.cards__text-buy')
+const cardText = document.querySelectorAll('.cards__text');
+const stuff = document.querySelectorAll('.cards__text--stuffing');
+const buy = document.querySelectorAll('.cards__text-buy');
+const outOfStock = document.querySelectorAll('.cards__text--out-of-stock');
+
 buyLink.forEach(function(link, i) {
   link.addEventListener('click', function() {
     var weight = cards[i].querySelector('.food-card__weight')
@@ -11,13 +13,18 @@ buyLink.forEach(function(link, i) {
   })
 })
 
-cards.forEach(function(card,i) {
+cards.forEach(function(card, i) {
   const weight = card.querySelector('.food-card__weight')
 
-  card.addEventListener('click', function() {
-    this.classList.toggle('food-card--selected')
-    weight.classList.toggle('food-card__weight--selected')
-    stuff[i].classList.toggle('js-inactive')
-    buy[i].classList.toggle('js-inactive')
-  })
+  // JQuery для проверки наличия класса (извините :))
+  if ($(outOfStock[i]).hasClass('js-active')) {
+    return false;
+  } else {
+    card.addEventListener('click', function() {
+      this.classList.toggle('food-card--selected')
+      weight.classList.toggle('food-card__weight--selected')
+      stuff[i].classList.toggle('js-inactive')
+      buy[i].classList.toggle('js-inactive')
+    })
+  }
 })
