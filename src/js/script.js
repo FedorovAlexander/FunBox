1382ba619846e518e99206790069ca1256e14889
alex <hamster2240@gmail.com>
2018-06-24 12:26:59 +0300

    Change card__text with click

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 const cards = document.querySelectorAll('.cards__item');
 const buyLink = document.querySelectorAll('.cards__text-link');
 const cardText = document.querySelectorAll('.cards__text')
+const stuff = document.querySelectorAll('.cards__text--stuffing')
+const buy = document.querySelectorAll('.cards__text-buy')
 buyLink.forEach(function(link, i) {
   link.addEventListener('click', function() {
     var weight = cards[i].querySelector('.food-card__weight')
@@ -9,11 +11,13 @@ buyLink.forEach(function(link, i) {
   })
 })
 
-cards.forEach(function(card) {
+cards.forEach(function(card,i) {
   const weight = card.querySelector('.food-card__weight')
-  const link = card.querySelector('.cards__text-link')
+
   card.addEventListener('click', function() {
     this.classList.toggle('food-card--selected')
     weight.classList.toggle('food-card__weight--selected')
+    stuff[i].classList.toggle('js-inactive')
+    buy[i].classList.toggle('js-inactive')
   })
 })
